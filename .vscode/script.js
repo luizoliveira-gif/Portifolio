31e545bcc676abe9fee87bbd646b2f3eeada7fd0
Luiz <lui.o.veiga@gmail.com>
2025-04-02 17:39:41 -0300

    :tada: initial commit

---
+++ .vscode/script.js
@@ -0,0 +1,13 @@
+function togglemode(){
+    const html= document.documentElement
+
+    html.classList.toggle('light')
+
+    /*if(html.classlist.contains('light')){
+        html.classList.remove('light')
+        }
+    else{
+         html.classList.add('light')
+        }
+*/
+}
